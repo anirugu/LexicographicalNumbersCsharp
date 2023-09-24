8932793143bfc298ce554ef03fd05975820fc534
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-09-24 16:38:27 +0530

    simplify the logic

--- Program.cs
+++ Program.cs
@@ -13,23 +13,6 @@ public class Solution
 {
     public IList<int> LexicalOrder(int n)
     {
-        List<int> value = new List<int>();
-        if (n > 0)
-            value.Add(0);
-        
-        for (int i = 0; i < 9; i++)
-        {
-            value.Add(i);
-        }
-    }
-
-    public IList<int> DSF(List<int> value, int start, int n)
-    {
-        for (int i = start * 10; i < n; i++)
-        {
-            value.Add(i);
-            
-        }
-        yield break;
+        return Enumerable.Range(1, n).OrderBy(x => x.ToString(), StringComparer.CurrentCultureIgnoreCase).ToList();
     }
 }
